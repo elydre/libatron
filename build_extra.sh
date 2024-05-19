3033ace82172fc2a7ffabb6a1760528a94f9eb9f
pf4 <74263128+elydre@users.noreply.github.com>
2024-05-19 18:03:59 +0200

    Update build_extra.sh

--- build_extra.sh
+++ build_extra.sh
@@ -8,3 +8,14 @@ bash build_generic.sh ../profanOS "-lpm"
 cp act.elf ../build
 cd ..
 rm -rf act
+
+rm -rf aledlang
+git clone https://github.com/elydre/aledlang.git
+
+echo "Building aledlang..."
+cp build_generic.sh aledlang
+cd aledlang
+bash build_generic.sh ../profanOS "-lpm"
+cp aledlang.elf ../build
+cd ..
+rm -rf aledlang
