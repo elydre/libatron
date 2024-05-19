4fc5ea94f9631ac8aa52e469760685e6ce6d9063
pf4 <74263128+elydre@users.noreply.github.com>
2024-05-19 18:22:22 +0200

    Update build_extra.sh

--- build_extra.sh
+++ build_extra.sh
@@ -15,7 +15,7 @@ git clone https://github.com/elydre/aledlang.git
 echo "Building aledlang..."
 cp build_generic.sh aledlang
 cd aledlang
-bash build_generic.sh ../profanOS "-lpm"
+bash build_generic.sh ../profanOS
 cp aledlang.elf ../build
 cd ..
 rm -rf aledlang
