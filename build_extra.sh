08a0480b536e7d12f58fc5b8cd28c7543662149c
pf4 <74263128+elydre@users.noreply.github.com>
2024-11-05 18:55:38 +0100

    Update build_extra.sh

--- build_extra.sh
+++ build_extra.sh
@@ -1,14 +1,3 @@
-rm -rf act
-git clone https://github.com/asqel/act.git
-
-echo "Building act..."
-cp build_generic.sh act
-cd act
-bash build_generic.sh ../profanOS "-lpm"
-cp act.elf ../build
-cd ..
-rm -rf act
-
 rm -rf aledlang
 git clone https://github.com/elydre/aledlang.git
 
