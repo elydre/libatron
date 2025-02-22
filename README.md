d0915df8d89c22e992517cebbb5246add150560c
pf4 <74263128+elydre@users.noreply.github.com>
2025-02-22 16:47:50 +0100

    libpng

--- README.md
+++ README.md
@@ -25,10 +25,10 @@ Find a list of all the libraries [here](https://github.com/elydre/profanOS/wiki/
 | **libcjson** | JSON parser | 1.7.17 | [here](https://github.com/DaveGamble/cJSON) | MIT |
 | **libgcc** | GCC runtime | 13.2.1 | [here](https://gcc.gnu.org/) | GPL-3.0 |
 | **libm** | Math library | openlibm 0.8.2 | [here](https://github.com/JuliaMath/openlibm) | MIT |
+| **libpng** | PNG image decoding library | 1.6.47 | [here](https://github.com/pnggroup/libpng) | libpng |
 | **liboe** | Additional functions | *sync* | [here](https://github.com/asqel/oeuf) | |
 | **libschrift** | Font rendering library | 0.10.2| [here](https://github.com/tomolt/libschrift) | ISC |
 | **libSDL2** | Simple Directmedia Layer | 2 | [here](https://github.com/libsdl-org/SDL/tree/SDL2) | as-is |
-| **libupng** | PNG image decoding library | 20100808 | [here](https://github.com/elanthis/upng) | as-is |
 | **libz** | Compression library | 1.3.1.1-motley | [here](https://github.com/openbsd/src/tree/master/lib/libz) | BSD |
 
 *I'm not the author of these commands or libraries, I just made them work on profanOS.*
---
+++ bil/libpng/build.sh
@@ -0,0 +1,63 @@
+rm -Rf libpng-1.6.47
+
+wget https://github.com/pnggroup/libpng/archive/refs/tags/v1.6.47.tar.gz
+tar -xvf v1.6.47.tar.gz
+rm -f v1.6.47.tar.gz
+
+cd libpng-1.6.47
+
+profan_path="../../../profanOS"
+
+cat << EOF > link.ld
+STARTUP($profan_path/out/make/entry_elf.o)
+ENTRY(_start)
+
+SECTIONS {
+    . = 0xC0000000;
+
+    .text : {
+        *(.text)
+        *(.text.*)
+    }
+
+    .rodata : {
+        *(.rodata)
+        *(.rodata.*)
+    }
+
+    .data : {
+        *(.data)
+        *(.data.*)
+    }
+
+    .bss : {
+        *(.bss)
+        *(.bss.*)
+    }
+}
+EOF
+
+export CC="../mycc"
+export CFLAGS="-m32 -Wno-discarded-qualifiers -ffreestanding -fno-exceptions -nostdinc -fno-stack-protector -D__profanOS__"
+export CPPFLAGS="-I $profan_path/include/zlibs -I $profan_path/include/addons"
+export LDFLAGS="-Wl,-melf_i386 -nostdlib -L$profan_path/out/zlibs -lc"
+
+./configure --prefix=/ \
+            --host=i686-linux-gnu \
+            --build=x86_64-linux-gnu \
+            CC="$CC" \
+            CFLAGS="$CFLAGS" \
+            CPPFLAGS="$CPPFLAGS" \
+            LDFLAGS="$LDFLAGS"
+
+sed -i 's/#define HAVE_FEENABLEEXCEPT 1/#undef HAVE_FEENABLEEXCEPT/' config.h
+
+make -j$(nproc)
+
+cp .libs/libpng16.so ../../../build/libpng16.so
+cp .libs/libpng16.a  ../../../build/libpng16.a
+
+tar -cf ../../../build/libpng_headers.tar png.h pngconf.h pnglibconf.h
+
+cd ..
+rm -Rf libpng-1.6.47
