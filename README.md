8ebbeff4623eb72b202c8cee2c70b29c6bca59d6
pf4 <74263128+elydre@users.noreply.github.com>
2026-05-22 11:45:01 +0200

    openssl

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ Additional libs, tools and commands for [profanOS](https://github.com/elydre/pro
 | **libschrift** | Font rendering library | 0.10.2| [here](https://github.com/tomolt/libschrift) | ISC |
 | **libSDL2** | Simple Directmedia Layer | 2 | [here](https://github.com/libsdl-org/SDL/tree/SDL2) | as-is |
 | **libxml2** | XML parsing library | 2.15.2 | [here](https://gitlab.gnome.org/GNOME/libxml2) | libxml2 |
+| **openssl** | TLS library | 4.0.0 | [here](https://github.com/openssl/openssl) | Apache-2.0 |
 | **zlib** | Compression library | 1.3.1.1-motley | [here](https://github.com/openbsd/src/tree/master/lib/libz) | BSD |
 
 Each packaged project is distributed under its own license, see the `_LICENCES/` directory for details.
---
+++ bil/openssl/build.sh
@@ -0,0 +1,45 @@
+profan_path="../../../profanOS"
+
+install_dir=$(realpath tmp_install)
+rm -Rf $install_dir openssl-4.0.0
+
+wget https://github.com/openssl/openssl/releases/download/openssl-4.0.0/openssl-4.0.0.tar.gz
+tar -xf openssl-4.0.0.tar.gz
+rm openssl-4.0.0.tar.gz
+
+cd openssl-4.0.0
+
+export CC="../mycc"
+export CFLAGS="-m32 -Wno-discarded-qualifiers -ffreestanding -fno-exceptions -nostdinc -fno-stack-protector -D__profanOS__ -DNO_SYS_PARAM_H -DNO_SYS_UN_H -DNO_SYSLOG -DOPENSSL_USE_IPV6=0"
+export CPPFLAGS="-I $profan_path/include/zlibs -I $profan_path/include/addons"
+export LDFLAGS="-Wl,-melf_i386 -nostdlib -L$profan_path/out/zlibs -lc"
+
+./config \
+    --prefix=$install_dir \
+    no-tests \
+    no-apps \
+    no-docs \
+    no-module \
+    no-dso \
+    no-threads \
+    no-ui-console \
+    no-autoload-config \
+    no-deprecated \
+    linux-x86
+
+make -j$(nproc)
+make install_dev # install_ssldirs install_sw
+
+cd $install_dir/include/openssl
+tar -czf ../../../../../build/openssl_headers.tar.gz *.h
+cd ../..
+
+cp lib/libcrypto.so ../../../build/libcrypto.so
+cp lib/libcrypto.a  ../../../build/libcrypto.a
+
+cp lib/libssl.so ../../../build/libssl.so
+cp lib/libssl.a  ../../../build/libssl.a
+
+cd ..
+
+rm -Rf $install_dir openssl-4.0.0
